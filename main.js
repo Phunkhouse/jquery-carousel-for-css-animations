cf5311063c23896f6f6ce3e5cbecbdc58f65d530
[DavidKrapka] <[krapka.david@icloud.com]>
2018-07-30 07:55:14 +0200

    Little refactoring

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 // SETTINGS
-let intervalDuration = 5000
-let amountOfSlides = 3
+const intervalDuration = 5000
+const amountOfSlides = 3
 
 // FUNCTIONALITY
 let index = 0
 
 setInterval( () => {
    index = (index + 1) % amountOfSlides
-
-   let lastSlide = amountOfSlides - 1
+   const lastSlide = amountOfSlides - 1
+   const showNext = () => {
+      $("[data-index='" + index + "']").stop().addClass("header__active")
+   }
 
    if (index >= 1) {
       $("[data-index='" + (index - 1) + "']").stop().removeClass("header__active")
-      $("[data-index='" + index + "']").stop().addClass("header__active")
+      showNext()
          
    // This resolved last iteration when carousel bugged on loop restart
    } else {
       $("[data-index='" + lastSlide + "']").stop().removeClass("header__active")
-         $("[data-index='" + index + "']").stop().addClass("header__active")
+      showNext()
    }
 
 }, intervalDuration)
